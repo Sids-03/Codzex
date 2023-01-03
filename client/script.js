d9baa22cad8cc41193a531fc8605b061e9ded8e5
Sids-03 <110683058+Sids-03@users.noreply.github.com>
2023-01-03 18:01:21 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv)
 
   //fetch data
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://codzex.onrender.com',{
       method: 'POST',
       headers: {
           'Content-Type': 'application/json',
